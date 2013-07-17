b6ce21a91f2c0c7d1e5b48493c4bed0858957ace
Richard Smith <richardsmith404@gmail.com>
2013-07-17 11:38:46 +0100

    moving relevant code from old version
    Former-commit-id: 2acf5891642fe4fe995d9e7ad89f77bee8078d1b

---
+++ lib/biopsy/objectivefunction.rb
@@ -0,0 +1,32 @@
+# Assembly Optimisation Framework: Objective Function
+# 
+# == Description
+# 
+# ObjectiveFunction is a skeleton parent class to ensure
+# objective functions provide the essential methods.
+# Because abstract classes don't really make sense in
+# Ruby's runtime compilation, we can only check if methods
+# are implemented at runtime (but at least we can raise
+# a sensible error)
+module BiOpSy
+
+  class ObjectiveFunction
+
+    # Runs the objective function for the assembly supplied,
+      # returning a real number value
+      #
+      # === Options
+      #
+      # * +:assemblydata+ - Hash containing data about the assembly to analyse
+      #
+      # === Example
+      #
+      # objective = ObjectiveFunction.new
+      # result = objective.run('example.fasta')
+    def run(assemblydata)
+      raise NotImplementedError.new("You must implement this")
+    end
+
+  end
+  
+end
