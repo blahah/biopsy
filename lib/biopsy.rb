a8f470a1b50feee2bea956c1a58e6b2337647065
Richard Smith <richardsmith404@gmail.com>
2013-07-29 10:33:54 +0100

    add which method to File
    Former-commit-id: edd7e8fcc79965736fdb633f1c06bb6f78854566

--- lib/biopsy.rb
+++ lib/biopsy.rb
@@ -1,5 +1,21 @@
 require "biopsy/version"
 
 module Biopsy
-  # Your code goes here...
+
+  class File
+
+    # extend the File class to add File::which method.
+    # returns the full path to the supplied cmd,
+    # if it exists in any location in PATH
+    def self.which(cmd)
+      exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+      ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+        exts.each { |ext|
+          exe = File.join(path, "#{cmd}#{ext}")
+          return exe if File.executable? exe
+        }
+      end
+      return nil
+    end
+
 end
