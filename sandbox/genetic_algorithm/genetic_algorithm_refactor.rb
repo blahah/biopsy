e26623f86f001131d1932726cbb002bfcc0a1a60
ParsaAkbari <parsa1.akbari@gmail.com>
2013-08-09 09:56:20 +0100

    Refactor wrapper around classes
    Former-commit-id: 35831436519cc0acbcecd4edc0dbc417f7fa9ef1

--- sandbox/genetic_algorithm/genetic_algorithm_refactor.rb
+++ sandbox/genetic_algorithm/genetic_algorithm_refactor.rb
@@ -1,5 +1,7 @@
-require 'pp'
 require 'csv'
+require 'pp'
+
+$global = 0
 
 class Generation
 	attr_reader :best, :population_homogenosity
@@ -9,7 +11,7 @@ class Generation
 		@population_size = population_size
 		@current_generation = []
 		@ranges = parameter_ranges
-		@MUTATION_RATE = 0.1
+		@MUTATION_RATE = 0.40
     @best = {
       :parameters => nil,
       :score => 0.0
@@ -31,15 +33,11 @@ class Generation
 	end
 
 	def run_generation
+		#pp @current_generation if $global%100 == 0
+		$global += 1
 		#homogeneous_test
-		#puts "STARTING GENERATION #{@current_generation.length}"
-		#pp @current_generation
 		selection_process
-		#puts "AFTER SELECTION GENERATION #{@current_generation.length}"
-		#pp @current_generation
 		crossover
-		#puts "AFTER CROSSOVER GENERATION #{@current_generation.length}"
-		#pp @current_generation
 
 		return @current_generation
 	end
@@ -230,21 +228,16 @@ class GeneticAlgorithm
 	end
 end
 
+##############################################################################
+##############################################################################
+##############################################################################
+##############################################################################
+##############################################################################
+##############################################################################
 
-
-
-
-parameters = {
-	:K => (21..77).step(8).to_a,
-	:M => (0..3).to_a, # def 1, min 0, max 3 #k value
-	:d => (0..6).step(2).to_a, # KmerFreqCutoff: delete kmers with frequency no larger than (default 0)
-	:D => (0..6).step(2).to_a, # edgeCovCutoff: delete edges with coverage no larger than (default 1)
-	:e => (2..12).step(5).to_a, # contigCovCutoff: delete contigs with coverage no larger than (default 2)
-	:t => (2..12).step(5).to_a, # locusMaxOutput: output the number of transcriptome no more than (default 5) in one locus
-}
 # load test set
 testset = {}
-
+$already_done = {}
 first = true
 head = nil
 all = []
@@ -272,7 +265,8 @@ def get_score (parameters, testset)
 	if $already_done[key.to_sym]
 		return $already_done[key.to_sym]
 	end
-	$iterid += 1
+	#puts "SCORE #{$no_iter}"
+	$no_iter += 1
 	score = 0
 	if testset.has_key? key
 	  unless key.split(':').size == 6
@@ -285,42 +279,100 @@ def get_score (parameters, testset)
 	return score
 end
 
-pop_size = 25
-res = ""
-count = 0
-$iterid = 0
-$already_done = {}
-csv_return = [["runid","iterid","K","M","d","D","e","t","generation_no","score", "homogenity"]]
-GA = []
-(1..10).each do |runid|
-	GA[runid] = GeneticAlgorithm.new(pop_size, parameters)
-	$iterid = 0
+##############################################################################
+##############################################################################
+##############################################################################
+##############################################################################
+##############################################################################
+##############################################################################
+
+
+# varied by param sweep
+parameters = {
+	:K => (21..77).step(8).to_a,
+	:M => (0..3).to_a, # def 1, min 0, max 3 #k value
+	:d => (0..6).step(2).to_a, # KmerFreqCutoff: delete kmers with frequency no larger than (default 0)
+	:D => (0..6).step(2).to_a, # edgeCovCutoff: delete edges with coverage no larger than (default 1)
+	:e => (2..12).step(5).to_a, # contigCovCutoff: delete contigs with coverage no larger than (default 2)
+	:t => (2..12).step(5).to_a, # locusMaxOutput: output the number of transcriptome no more than (default 5) in one locus
+}
+mutation_rate = 0.3
+pop_size = 10
+
+# varied depending on how much time available
+max_runs = 100
+max_iter = 100
+
+# output variables
+no_fail = 0
+no_done = 0
+avg_iter = 0
+total_iter = 0
+
+$test = 0
+
+(1..max_runs).each do |no_runs|
+	puts "Run number: #{no_runs-1} Total iterations: #{total_iter} Iterations this run: #{$already_done.length}" #if no_runs%10 == 0
+	# variables used during runs
+	$no_iter = 0
+	now_break = false
+	first = true
 	res = ""
+	gen_Al = GeneticAlgorithm.new(pop_size, parameters)
+	count = 0
 	$already_done = {}
-	#csv_return << []
-	(1..10).each do |num|
-		if res.is_a? Array
-			count += 1
-			#puts "runid: #{runid} iterid: #{$iterid} params: #{GA[runid].best[:parameters].map {|key, value| value}} generation: #{num} score: #{GA[runid].best[:score]} homog: #{GA[runid].get_homog}" #if count%10 == 0
-			csv_return << [runid, $iterid] + GA[runid].best[:parameters].map {|k, v| v} + [num, GA[runid].best[:score], GA[runid].get_homog]
+
+	# generation loop
+	while true == true do 
+
+		# break generation if max iter reached
+		break if now_break == true
+
+		# if first population has been inserted (res will also be an array)
+		if first == false
+			# loop through the resultant population adding members to the new generation
 			res_temp = Marshal.load(Marshal.dump(res))
 			res_temp.each do |parameter_set|
-				res = GA[runid].run_one_iteration(parameter_set[:parameters], get_score(parameter_set[:parameters], testset))
+				#print "run"
+				res = gen_Al.run_one_iteration(parameter_set[:parameters], get_score(parameter_set[:parameters], testset))
+				# if max iter reached break current loop and generation loop
+				if $no_iter == max_iter
+					#puts "fail break #{$no_iter} #{gen_Al.best}"
+					no_fail += 1
+					now_break = true
+					total_iter += $no_iter
+					break
+				end
 			end
+			count += 1
+			#puts "\nrunid: #{no_runs} iterid: #{$no_iter} params: #{gen_Al.best[:parameters].map {|key, value| value}} score: #{gen_Al.best[:score]}" #if count%3 == 0
+			# if best score reached break generation loop
+			if gen_Al.best[:score] == 591
+				no_done += 1
+				avg_iter += $no_iter
+				total_iter += $no_iter
+				break
+			end
+		# if first population has not been inserted
 		else
+			# insert the first population set
 			(1..pop_size).each do |n|
-				parameter_set = GA[runid].generate_chromosome
-				score = get_score(parameter_set, testset)
-				res = GA[runid].run_one_iteration(parameter_set, score)
+				parameter_set = gen_Al.generate_chromosome
+				res = gen_Al.run_one_iteration(parameter_set, get_score(parameter_set, testset))
 			end
-			#puts "runid: #{runid} iterid: #{$iterid} params: #{GA[runid].best[:parameters].map {|key, value| value}} generation: #{num} score: #{GA[runid].best[:score]} homog: #{GA[runid].get_homog}" #if count%10 == 0
-			csv_return << [runid, $iterid] + GA[runid].best[:parameters].map {|k, v| v} + [num, GA[runid].best[:score], GA[runid].get_homog]
+			# the first population set has been inserted, update variable
+			first = false
+			# if best score reached break generation loop now
+			if gen_Al.best[:score] == 591
+				no_done += 1
+				avg_iter += $no_iter
+				total_iter += $no_iter
+				break
+			end
 		end
 	end
 end
 
-CSV.open('dataset.csv', 'w') do |csv_file|
-	csv_return.each do |line|
-		csv_file << line
-	end
-end
+
+
+puts "no_done: #{no_done}, no_fail: #{no_fail}, percentage: #{no_done.to_f/max_runs}, avg iter: #{avg_iter/no_done}, total iter: #{total_iter/max_runs}"
