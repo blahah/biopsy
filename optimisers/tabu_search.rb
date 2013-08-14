e704f7cb6dc86ad97c907a9f5c36db61f775b2b1
Richard Smith <richardsmith404@gmail.com>
2013-08-14 12:58:50 +0100

    remove testing code, expose algorithm parameters
    Former-commit-id: d46d065e7c5ae2331e6ce2b2ab87a377c3f6ea1f

--- optimisers/tabu_search.rb
+++ optimisers/tabu_search.rb
@@ -19,11 +19,11 @@ module BiOpSy
     include Methadone::CLILogging
 
     # create a new Distribution
-    def initialize(mean, range, increment)
+    def initialize(mean, range, sd_increment_proportion, starting_sd_divisor)
       @mean = mean
-      @sd = range.size / 30.0 # this is arbitrary - should we learn a good initial setting?
+      @sd = range.size / starting_sd_divisor # this is arbitrary - should we learn a good initial setting?
       @range = range
-      @increment = increment
+      @sd_increment_proportion = sd_increment_proportion
       self.generate_distribution
     end
 
@@ -35,14 +35,14 @@ module BiOpSy
     # loosen the distribution by increasing the sd
     # and renerating
     def loosen
-      @sd += @increment * @range.size
+      @sd += @sd_increment_proportion * @range.size
       self.generate_distribution
     end
 
     # tighten the distribution by reducing the sd
     # and regenerating
     def tighten
-      @sd -= @increment * @range.size unless (@sd <= 0.5)
+      @sd -= @sd_increment_proportion * @range.size unless (@sd <= 0.5)
       self.generate_distribution
     end
 
@@ -144,10 +144,12 @@ module BiOpSy
     include Methadone::CLILogging
 
     attr_reader :current, :best, :hood_no
+    attr_writer :max_hood_size, :sd_increment_proportion, :starting_sd_divisor, :backtrack_cutoff
 
     def initialize(parameter_ranges, threads=8, limit=nil)
+
       @ranges = parameter_ranges
-      # self.set_term_conditions time_limit
+
       # solution tracking
       @current = Hash[parameter_ranges.map { |param, range| [param, range.sample] }]
       @best = {:score => 0}
@@ -156,11 +158,19 @@ module BiOpSy
       @tabu = Set.new
       @tabu_limit = nil
       @start_time = Time.now
+
       # neighbourhoods
       @max_hood_size = 50
+      @starting_sd_divisor = 30
+      @sd_increment_proportion = 0.1
       self.define_neighbourhood_structure
       @current_hood = BiOpSy::Hood.new(@distributions, @max_hood_size, @tabu)
       @hood_no = 1
+
+      # backtracking
+      @iterations_since_best = 0
+      @backtrack_cutoff = @max_hood_size * 3
+
     end # initialize
 
     # if not being controlled by RunController, #run
@@ -185,7 +195,11 @@ module BiOpSy
 
     def update_best? current
       @current_hood.update_best? current
-      @best = @current if @current[:score] > @best[:score]
+      if @current[:score] > @best[:score]
+        @best = @current
+      else
+        @iterations_since_best += 1
+      end
     end
 
     # use probability distributions to define the
@@ -193,21 +207,39 @@ module BiOpSy
     def define_neighbourhood_structure
       # probabilities
       @distributions = {}
-      @increment = 0.1 # proportion of the range by which to increment the SD
       @current.each_pair do |param, value|
         mean = @ranges[param].index(value)
         range = @ranges[param]
-        @distributions[param] = BiOpSy::Distribution.new(mean, range, @increment)
+        @distributions[param] = BiOpSy::Distribution.new(mean, 
+                                                        range,
+                                                        @sd_increment_proportion,
+                                                        @starting_sd_divisor)
       end
     end
 
     # update the neighbourhood structure by adjusting the probability
     # distributions according to total performance of each parameter
     def update_neighbourhood_structure
-      @current_hood.best[:parameters].each_pair do |param, value|
+      self.backtrack_or_continue()[:parameters].each_pair do |param, value|
         mean = @ranges[param].index(value)
         range = @ranges[param]
-        @distributions[param] = BiOpSy::Distribution.new(mean, range, @increment)
+        @distributions[param] = BiOpSy::Distribution.new(mean, 
+                                                        range,
+                                                        @sd_increment_proportion,
+                                                        @starting_sd_divisor)
+      end
+    end
+
+    # return the correct 'best' location to form a new neighbourhood around
+    # deciding whether to continue progressing from the current location
+    # or to backtrack to a previous good location to explore further
+    def backtrack_or_continue
+      if @iterations_since_best * @max_hood_size >= @backtrack_cutoff
+        @iterations_since_best = 0
+        debug('backtracked to best')
+        @best
+      else
+        @current_hood.best
       end
     end
 
@@ -244,148 +276,3 @@ module BiOpSy
   end # TabuSearch
 
 end # BiOpSy
-
-
-# # simple test (move to real testing env soon)
-# ranges = {
-#   :a => (1..100).to_a,
-#   :b => (1..100).to_a,
-#   :c => (1..50).to_a
-# }
-
-# #######
-# # simple test with convex three-parameter function
-# #######
-
-# tabu = BiOpSy::TabuSearch.new(ranges) 
-
-# def fake_objective(a, b, c)
-#   # should be easy - convex function taken from http://www.economics.utoronto.ca/osborne/MathTutorial/CVNF.HTM
-#   #  f (x1, x2, x3) = x12 + 2x22 + 3x32 + 2x1x2 + 2x1x3
-#   # optimum is a=100, b=100, c=50, score=57800
-#   a**2 + 2 * (b**2) + 3 * (c**2) + 2 * (a * b) + 2 * (a + c)
-# end
-
-# p tabu.current
-
-# res = []
-
-# (1..10000).each do |i|
-#   a, b, c = tabu.current[:a], tabu.current[:b], tabu.current[:c]
-#   score = fake_objective(a, b, c)
-#   # puts "a:#{a}, b:#{b}, c:#{c} => #{score}"
-#   tabu.run_one_iteration(tabu.current, score)
-#   res << [tabu.best, tabu.hood_no]
-# end
-
-# require 'csv'
-# CSV.open('fake_objective_opt.csv', 'w') do |csv|
-#   csv << %w(a b c hood_no score)
-#   res.each do |r, t|
-#     csv << r[:parameters].map { |k, v| v } + [t, r[:score]]
-#   end
-# end
-
-# p tabu.best
-
-########
-# test with n50 for SOAPdt dataset
-########
-require 'csv'
-
-# set parameters
-parameters = {
-  :K => (21..77).step(8).to_a,
-  :M => (0..3).to_a, # def 1, min 0, max 3 #k value
-  :d => (0..6).step(2).to_a, # KmerFreqCutoff: delete kmers with frequency no larger than (default 0)
-  :D => (0..6).step(2).to_a, # edgeCovCutoff: delete edges with coverage no larger than (default 1)
-  :e => (2..12).step(5).to_a, # contigCovCutoff: delete contigs with coverage no larger than (default 2)
-  :t => (2..12).step(5).to_a, # locusMaxOutput: output the number of transcriptome no more than (default 5) in one locus
-}
-
-# load test set
-testset = {}
-
-first = true
-head = nil
-all = []
-metrics = {
-  'n50' => 591,
-  'largest' => 2105,
-  'rba_result' => 839,
-  'brm_paired' => 34428,
-}
-
-CSV.open('../../../sandbox/soapdt_sweep/test_set.csv', 'r').each do |line|
-  if first
-    head = line.map { |s| s.to_sym }[0..5]
-    first = false
-    next
-  end
-  key = line[0..5].join(':')
-  value = Hash[%w(n50 largest rba_result brm_paired).zip(line[6..-1].map { |v| v.to_i })]
-  testset[key] = value
-end
-
-# setup
-tabu = nil
-
-# run
-res = {} # store the full output of each runlargest, rba_result, brm_paired, ut_result
-
-opt_iter = {} # store the iteration at which the optimum was reached
-
-num_repeats = 100
-max_iterations = 1000
-
-metrics.each_pair do |metric, opt|
-  puts "testing #{metric} with #{num_repeats} repeats of max #{max_iterations} iterations"
-  mres = []
-  mopt_iter = []
-  (1..100).each do |runid|
-    tabu = BiOpSy::TabuSearch.new(parameters)
-    (1..1000).each do |iterid|
-      key = head.map { |s| tabu.current[s] }.join(':')
-      score = 0
-      if testset.has_key? key
-        unless key.split(':').size == 6
-          p "key not found: #{key}" 
-          p "current: #{tabu.current}"
-        end
-        score = testset[key][metric]
-      end
-      tabu.run_one_iteration(tabu.current, score)
-      if tabu.best.has_key? :parameters
-        mres << [tabu.best, tabu.hood_no, runid, iterid]
-      end
-      if tabu.best[:score] == opt
-        mopt_iter << iterid
-        break
-      end
-    end
-  end
-  res[metric] = mres
-  opt_iter[metric] = mopt_iter
-end
-
-res.each_pair do |metric, mres|
-  CSV.open("#{metric}.csv", 'w') do |csv|
-    csv << %w(runid iterid K M d D e t hood_no score)
-    mres.each do |r, t, runid, iterid|
-      begin
-        csv << [runid, iterid] + r[:parameters].map { |k, v| v } + [t, r[:score]]
-      rescue
-        p r
-      end
-    end
-  end
-end
-
-opt_iter.each_pair do |metric, mopt_iter|
-  CSV.open("#{metric}_opt_iter.csv", 'w') do |csv|
-    csv << ['mopt_iter']
-    mopt_iter.each do |m|
-      csv << [m]
-    end
-  end
-end
